sipwise/cdr-exporter

commit 2c31646433ff46d465f99a4ec6e17180f443d582

MT#8969 fix perl warnings:
Bareword file handle opened at line 28, column 1. See pages 202,204 of PBP. (Severity: 5)
Two-argument "open" used at line 28, column 1. See page 207 of PBP. (Severity: 5)
Integer with leading zeros: "0777" at line 285, column 48. See page 58 of PBP. (Severity: 5)
Integer with leading zeros: "0666" at line 309, column 46. See page 58 of PBP. (Severity: 5)

Two-argument "open" used at line 64, column 1. See page 207 of PBP. (Severity: 5)
Integer with leading zeros: "0666" at line 247, column 71. See page 58 of PBP. (Severity: 5)

Change-Id: Ic1eae412e4e39e8fd8a41537b9ecf846d4814f9a

cdr-exporter.pl
@@ -25,9 +25,9 @@ our $FILES_MASK = '022';
 
 
 my $config_file = "/etc/ngcp-cdr-exporter/cdr-exporter.conf";
-open CONFIG, "$config_file" or die "Program stopping, couldn't open the configuration file '$config_file'.\n";
+open my $CONFIG, '<', "$config_file" or die "Program stopping, couldn't open the configuration file '$config_file'.\n";
 
-while (<CONFIG>) {
+while (<$CONFIG>) {
     chomp;                  # no newline
     s/#.*//;                # no comments
     s/^\s+//;               # no leading white
@@ -37,7 +37,7 @@ while (<CONFIG>) {
         no strict 'refs';
         $$var = $value;
 }
-close CONFIG;
+close $CONFIG;
 
 
 
@@ -282,7 +282,7 @@ my @CDR_RESELLER_BODY_FIELDS = qw(
 			for my $dd (@dirlist) {
 				if (! -d $dd) {
 					mkdir($dd) or die("failed to create target directory $dd ($!), stop");
-					chownmod($dd, $FILES_OWNER, $FILES_GROUP, 0777, $FILES_MASK);
+					chownmod($dd, $FILES_OWNER, $FILES_GROUP, '0777', $FILES_MASK);
 				}
 			}
 			my $fn = sprintf('%s/%s_%s_%s_%010i.cdr', $dircomp, $PREFIX, $VERSION, $ts, $MARKS{lastseq});
@@ -306,7 +306,7 @@ my @CDR_RESELLER_BODY_FIELDS = qw(
 
 			rename($tfn, $fn) or die("failed to move tmp-file $tfn to $fn ($!), stop");
 			print("### successfully moved $tfn to $fn\n");
-			chownmod($fn, $FILES_OWNER, $FILES_GROUP, 0666, $FILES_MASK);
+			chownmod($fn, $FILES_OWNER, $FILES_GROUP, '0666', $FILES_MASK);
 		}
 
 		# update exported cdrs

event-exporter.pl
@@ -61,7 +61,7 @@ foreach my $cp(@config_paths) {
 die "Config file $cf not found in path " . (join " or ", @config_paths) . "\n"
     unless $config_file;
 
-open my $CONFIG, "$config_file" or die "Couldn't open the configuration file '$config_file'.\n";
+open my $CONFIG, '<', "$config_file" or die "Couldn't open the configuration file '$config_file'.\n";
 
 while (<$CONFIG>) {
     chomp;                  # no newline
@@ -244,7 +244,7 @@ foreach my $file(readdir($fh)) {
     if(-f $src) {
         DEBUG "### moving $src to $dst\n";
         copy($src, $dst);
-        NGCP::CDR::Export::chownmod($dst, $FILES_OWNER, $FILES_GROUP, 0666, $FILES_MASK);
+        NGCP::CDR::Export::chownmod($dst, $FILES_OWNER, $FILES_GROUP, '0666', $FILES_MASK);
         if($TRANSFER_TYPE eq "sftp") {
             NGCP::CDR::Transfer::sftp(
                 $dst, $TRANSFER_HOST, $TRANSFER_PORT, 

commit edfaf05f8b25a644359ff4e2c8fa8ba88ec853a9

MT#8969 fix file permissions
Fixing regression from commit 2c31646:
mode "0644" sets mode to --w----r-T as perdoc -f chmod explains.
See PBP pages 58-59 for details.

cdr-exporter.pl
@@ -273,7 +273,8 @@ my @CDR_RESELLER_BODY_FIELDS = qw(
 				if (! -d $dd) {
 					mkdir($dd) or die("failed to create target directory $dd ($!), stop");
 					chownmod($dd, $config->{'default.FILES_OWNER'},
-						$config->{'default.FILES_GROUP'},	'0777', $config->{'default.FILES_MASK'});
+						$config->{'default.FILES_GROUP'},
+						oct(777), $config->{'default.FILES_MASK'});
 				}
 			}
 			my $fn = sprintf('%s/%s_%s_%s_%010i.cdr', $dircomp,
@@ -303,7 +304,7 @@ my @CDR_RESELLER_BODY_FIELDS = qw(
 			print("### successfully moved $tfn to $fn\n");
 			chownmod($fn, $config->{'default.FILES_OWNER'},
 				$config->{'default.FILES_GROUP'},
-				'0666', $config->{'default.FILES_MASK'});
+				oct(666), $config->{'default.FILES_MASK'});
 		}
 
 		# update exported cdrs

event-exporter.pl
@@ -224,7 +224,7 @@ foreach my $file(readdir($fh)) {
         DEBUG "### moving $src to $dst\n";
         copy($src, $dst);
         NGCP::CDR::Export::chownmod($dst, $config->{'default.FILES_OWNER'},
-            $config->{'default.FILES_GROUP'}, '0666',
+            $config->{'default.FILES_GROUP'}, oct(666),
             $config->{'default.FILES_MASK'});
         if($config->{'default.TRANSFER_TYPE'} eq "sftp") {
             NGCP::CDR::Transfer::sftp(