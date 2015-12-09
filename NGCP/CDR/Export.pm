514442260cbdc9a0484cb853a2b0c5672f42b5f0
Richard Fuchs <rfuchs@sipwise.com>
2015-12-09 08:36:05 -0500

    MT#14459 implement legacy export options
    Change-Id: I90503a88e0ea5a6ed35fd08e526beb70129963cf

--- NGCP/CDR/Export.pm
+++ NGCP/CDR/Export.pm
@@ -107,6 +107,7 @@ sub chownmod {
 sub write_file {
     my (
         $lines, $dircomp, $prefix, $version, $ts, $lastseq, $suffix,
+        $format, $file_data,
     ) = @_;
 
     my $fn =  sprintf('%s/%s_%s_%s_%010i.%s', $dircomp, $prefix, $version, $ts, $lastseq, $suffix);
@@ -116,16 +117,30 @@ sub write_file {
     my $ctx = Digest::MD5->new;
 
     my $num = @{ $lines };
-    unshift(@{ $lines }, sprintf('%s,%04i', $version, $num));
+    if ($format eq 'kabelplus') {
+            unshift(@{ $lines }, "'$num'". ',' x 15 ."'hdr',,,'$$file_data[0]','$$file_data[1]',,,'$$file_data[2]'," .
+			        "'$$file_data[3]','$$file_data[4]'". ',' x 10);
+    }
+    else {
+            unshift(@{ $lines }, sprintf('%s,%04i', $version, $num));
+    }
+
+    my $nl = "\n";
+    $format eq 'kabelplus' and $nl = "\r\n";
 
     for my $l (@{ $lines }) {
-        my $ol = "$l\n";
+        my $ol = "$l$nl";
         print $fd ($ol);
         $ctx->add($ol);
     }
 
     my $md5 = $ctx->hexdigest;
-    print $fd ("$md5\n");
+    if ($fileformat eq 'kabelplus') {
+            print $fd (",,'$md5'". ',' x 13 ."'md5'". ',' x 19 . "$nl");
+    }
+    else {
+            print $fd ("$md5$nl");
+    }
 
     print("### $num data lines written to $tfn, checksum is $md5\n");
     close($fd) or die ("failed to close tmp-file $tfn ($!), stop");
