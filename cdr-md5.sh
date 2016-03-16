b77c67fd94cc963532dd6fff74cdcf3a512eeec4
Guillem Jover <gjover@sipwise.com>
2016-03-16 12:27:11 +0100

    TT#49101 Use mktemp instead of creating an insecure temporary file
    Change-Id: Ia57d821a27077a38c4987f87167ecb3bec011587

--- cdr-md5.sh
+++ cdr-md5.sh
@@ -2,7 +2,8 @@
 error() { echo "$@"; exit 1; }
 test -n "$1" || error "Usage: $0 <cdr-file>"
 test -f "$1" || error "File '$1' not found"
-TMPFILE="/tmp/$(basename "$1").$$"
+BASECDR="$(basename "$1")"
+TMPFILE="$(mktemp -t "cdr-md5.${BASECDR}.XXXXXXXXXX")"
 MD5="$(tail -1 "$1")  ${TMPFILE}"
 sed '$d' < "$1" > "${TMPFILE}"
 echo "$MD5" | md5sum -c -
