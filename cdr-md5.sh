a1cb9ca45d0b3ea9dc8eea75d5a4d7a7b7e35553
Alexander Lutay <alutay@sipwise.com>
2017-02-03 01:36:00 +0100

    TT#10254 Fix shellcheck warnings for cdr-md5.sh
    Change-Id: I18cb52207614c8466ecf1b789d29ef39da54ed0d
    (cherry picked from commit fc01d12fd3107faaa0de9d1d527532ad8e93e235)

--- cdr-md5.sh
+++ cdr-md5.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-error() { echo $@; exit 1; }
+error() { echo "$@"; exit 1; }
 test -n "$1" || error "Usage: $0 <cdr-file>"
 test -f "$1" || error "File '$1' not found"
 TMPFILE="/tmp/$(basename "$1").$$"
-MD5="$(tail -1 "$1")  $TMPFILE"
-sed '$d' < "$1" > $TMPFILE
+MD5="$(tail -1 "$1")  ${TMPFILE}"
+sed '$d' < "$1" > "${TMPFILE}"
 echo "$MD5" | md5sum -c -
-rm -f $TMPFILE
+rm -f "${TMPFILE}"
