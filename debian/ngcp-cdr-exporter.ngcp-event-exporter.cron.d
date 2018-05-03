8250abc79a3cb33b61c282fc30c5ef17adc47f63
Guillem Jover <gjover@sipwise.com>
2018-05-03 15:40:24 +0200

    TT#36351 Use modern ngcp-check-active
    Change-Id: I838350dcdc275c43dc9a8f102259b4cc6086898b

--- debian/ngcp-cdr-exporter.ngcp-event-exporter.cron.d
+++ debian/ngcp-cdr-exporter.ngcp-event-exporter.cron.d
@@ -1,1 +1,1 @@
-0 0 * * *   root    . /etc/default/ngcp-roles; if /usr/sbin/ngcp-check_active -q && [ "$NGCP_IS_MGMT" = "yes" ] ; then /usr/sbin/event-exporter >/dev/null; fi
+0 0 * * *   root    . /etc/default/ngcp-roles; if /usr/sbin/ngcp-check-active -q && [ "$NGCP_IS_MGMT" = "yes" ] ; then /usr/sbin/event-exporter >/dev/null; fi
