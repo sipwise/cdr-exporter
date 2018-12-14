c2cfa6929cc1561dfd8d63946ebb777ef9a94697
Guillem Jover <gjover@sipwise.com>
2018-12-14 19:32:52 +0100

    TT#49102 Namespace binaries with ngcp-
    This makes it obvious these are NGCP specific programs, and avoids
    possible collisions with other third party programs.
    
    Create backward compatibility symlinks for a smoother transition.
    
    Change-Id: I7040725f16f835307b3ac8885186e7851e596c90

--- debian/ngcp-cdr-exporter.cron.d
+++ debian/ngcp-cdr-exporter.cron.d
@@ -1,1 +1,1 @@
-25,55 * * * *   root    . /etc/default/ngcp-roles; if /usr/sbin/ngcp-check-active -q && [ "$NGCP_IS_MGMT" = "yes" ] ; then /usr/sbin/cdr-exporter >/dev/null; fi
+25,55 * * * *   root    . /etc/default/ngcp-roles; if /usr/sbin/ngcp-check-active -q && [ "$NGCP_IS_MGMT" = "yes" ] ; then /usr/sbin/ngcp-cdr-exporter >/dev/null; fi
